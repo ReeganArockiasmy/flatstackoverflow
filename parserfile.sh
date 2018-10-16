cd502faf66d744f7efbbbd6f11f9d271f346396e
your name <yor eMail adress>
2018-10-16 08:03:53 +0200

    add parserfile.sh -t own -t bash

--- parserfile.sh
+++ parserfile.sh
@@ -3,6 +3,7 @@
 #jshon -l < stackoverflow.txt 
 
 #echo $loopvar
+# jshon -a 1 -e answer
 
 # for index in $( jshon -k < stackoverflow.txt )
 # do
@@ -31,8 +32,14 @@ list_questions() {
 	# question=`jshon -e $index -e question < $filename`
 	# question=$(echo $question | sed 's/^.\(.*\).$/\1/'| sed 's/^.\(.*\).$/\1/' | sed 's/^.\(.*\).$/\1/')
 	# echo $index $question
-	printf $index
-	jshon -e $index -e question < $filename | sed -e '/^\[$/d' -e  '/^\]$/d' -e 's/\,$//' -e 's/\\//g' -e 's/^ "/ /g' -e 's/"$//g'
+	
+	# if jshon -e $index -e tags | grep -q  $tags
+	#    then
+	if jshon -e $index -e tags < $filename | grep -q $tags
+	then
+	       printf $index
+	       jshon -e $index -e question < $filename | sed -e '/^\[$/d' -e  '/^\]$/d' -e 's/\,$//' -e 's/\\//g' -e 's/^ "/ /g' -e 's/"$//g'
+	fi
     done 
    
 }
@@ -52,7 +59,55 @@ done | sort | uniq
 
 }
 
-#list_tags
-list_questions
+
+# list_tags
+# list_questions
 # read index
 # view_solution
+
+usage(){
+
+    echo " --all | -a {tags,questions}"
+}
+
+
+
+if [ $# -gt 0 ];
+then
+    while [ "$1" != "" ]; do
+	case $1 in
+            -f | --file )           shift 
+                                    filename=$1
+                                    ;;
+	    -a | --all )
+			shift
+			key=$1
+			list_tags
+			;;
+			
+            -t | --tags )   
+			     shift
+			     tags=" -e "$1$tags
+                                    ;;
+            -h | --help )           usage
+                                    exit
+                                    ;;
+            * )                     usage
+                                    exit 1
+	esac
+	shift
+    done
+else
+    echo "your command line"
+fi
+
+
+if [ ! -z ${tags+x} ];
+then
+    list_questions
+fi
+
+
+
+cat stackoverflow.txt | jshon -a tags | grep$tags
+echo "grep"$tags
